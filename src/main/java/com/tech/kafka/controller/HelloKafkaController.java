5fec8b49aa4ed3987e336f889b9d8a792cc0c348
Saurav Kumar <kumar_sauravsk@outlook.com>
2019-12-15 22:13:10 +0530

    Initial commit

---
+++ src/main/java/com/tech/kafka/controller/HelloKafkaController.java
@@ -0,0 +1,89 @@
+package com.tech.kafka.controller;
+
+
+import com.tech.kafka.service.Producer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/kafka")
+public class HelloKafkaController {
+    private static final Logger logger =
+            LoggerFactory.getLogger(HelloKafkaController.class);
+
+    @Autowired
+    private KafkaAdmin admin;
+
+
+    private final Producer producer;
+
+    @Autowired
+    HelloKafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+    private KafkaMessageListenerContainer<Integer, String> createContainer(
+            ContainerProperties containerProps) {
+        Map<String, Object> props = consumerProps();
+        DefaultKafkaConsumerFactory<Integer, String> cf =
+                new DefaultKafkaConsumerFactory<Integer, String>(props);
+        KafkaMessageListenerContainer<Integer, String> container =
+                new KafkaMessageListenerContainer<>(cf, containerProps);
+        return container;
+    }
+
+    private KafkaTemplate<Integer, String> createTemplate() {
+        Map<String, Object> senderProps = senderProps();
+        ProducerFactory<Integer, String> pf =
+                new DefaultKafkaProducerFactory<Integer, String>(senderProps);
+        KafkaTemplate<Integer, String> template = new KafkaTemplate<>(pf);
+        return template;
+    }
+
+    private Map<String, Object> consumerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        Object group = "localhost:9092";
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, group);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return props;
+    }
+
+    private Map<String, Object> senderProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+}
---
+++ src/main/java/com/tech/kafka/controller/User.java
@@ -0,0 +1,12 @@
+package com.tech.kafka.controller;
+
+public class User {
+
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
